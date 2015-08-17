9bca3371fc12aa289ebda8e424561894332a7ba6
PrahlM93 <matt_prahl@live.com>
2015-08-17 19:03:23 -0400

    Added tests for PowerShell

--- tests/cli/test_cli_requests.py
+++ tests/cli/test_cli_requests.py
@@ -24,6 +24,12 @@ class TestCliGetFunctions:
         print rv.data
         assert "127.0.0.1" in rv.data
 
+    def test_ip_powershell(self, client):
+        rv = client.get("/",
+                        environ_base={'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) WindowsPowerShell/5.0.10240.16384', 'REMOTE_ADDR': '127.0.0.1'})
+        print rv.data
+        assert "127.0.0.1" in rv.data
+
     def test_root_wget(self, client):
         rv = client.get("/all",
                         environ_base={'HTTP_USER_AGENT': 'Wget/1.16.3 (linux-gnu)', 'REMOTE_ADDR': '127.0.0.1'})
@@ -39,6 +45,12 @@ class TestCliGetFunctions:
     def test_root_fetch(self, client):
         rv = client.get("/all",
                         environ_base={'HTTP_USER_AGENT': 'fetch libfetch/2.0', 'REMOTE_ADDR': '127.0.0.1'})
+        print rv.data
+        assert json.loads(rv.data)
+
+    def test_root_powershell(self, client):
+        rv = client.get("/all",
+                        environ_base={'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) WindowsPowerShell/5.0.10240.16384', 'REMOTE_ADDR': '127.0.0.1'})
         print rv.data
         assert json.loads(rv.data)
 
